5f8e0d4e91e6bd23085f1b23f841afa93928551d
yingmule <534446340@qq.com>
2018-11-26 20:45:27 +0800

    CAS

--- src/test/java/lessons/threads/lesson/CASDemo.java
+++ src/test/java/lessons/threads/lesson/CASDemo.java
@@ -4,6 +4,6 @@ import java.lang.annotation.Native;
 
 public class CASDemo {
     public static void main(String[] args) {
-        System.out.println("CAS");
+        System.out.println("CASS");
     }
 }
