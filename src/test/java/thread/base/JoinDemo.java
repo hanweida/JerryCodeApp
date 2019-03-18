2de9579e2ac217bf9a842889fdf535c8ec67a6f8
yingmule <534446340@qq.com>
2019-03-18 09:53:14 +0800

    readwritelock

--- src/test/java/thread/base/JoinDemo.java
+++ src/test/java/thread/base/JoinDemo.java
@@ -6,8 +6,8 @@ public class JoinDemo {
 
     /**
      * Thread.join
-     * 当前线程需要等待 调用join的线程，执行完毕后，才能执行
-     * 下面的例子：启动10个线程，当主线程执行遇到 创建的线程thread.join()时，就会等待创建线程执行完，才会继续执行
+     * 父线程调用子线程的对象 subThread.join()，需要等子线程执行完成，才会执行
+     * 下面的例子：启动10个线程，当主线程中执行遇到 创建的线程thread.join()时，就会等待创建线程执行完，才会继续执行
      */
     @Test
     public void demo(){
--- src/test/java/thread/concurrent/lock/readwritelock/ReentrantReadWriteLockDemo.java
+++ src/test/java/thread/concurrent/lock/readwritelock/ReentrantReadWriteLockDemo.java
@@ -4,18 +4,76 @@ import org.junit.Test;
 
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
+
+
 public class ReentrantReadWriteLockDemo {
+  int a = 0;
+  @Test
+  public void demo(){
+    ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+    ReentrantReadWriteLock.ReadLock readLock = readWriteLock.readLock();
+    ReentrantReadWriteLock.WriteLock writeLock = readWriteLock.writeLock();
 
-    @Test
-    public void demo(){
-        ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
-//        Thread thread  = new Thread(new Runnable() {
-//            public void run() {
-//                System.out.println(Thread.currentThread().getName());
-//
-//            }
-//        });
+    Thread readWriteThread = new Thread(new Runnable() {
+      @Override
+      public void run() {
+        System.out.println(Thread.currentThread().getName() +"：" + "invoke");
+        readLock.lock();
+        System.out.println(Thread.currentThread().getName() +"：" + "getreadlock");
+        System.out.println(Thread.currentThread().getName() +"：" + "Sleep");
+        try {
+          Thread.sleep(10000);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread() + "：" + a);
+        readLock.unlock();
+      }
+    });
 
+    Thread readWriteThread2 = new Thread(new Runnable() {
+      @Override
+      public void run() {
+        System.out.println(Thread.currentThread().getName() +"：" + "invoke");
+        readLock.lock();
+        System.out.println(Thread.currentThread().getName() +"：" + "getreadlock");
+        System.out.println(Thread.currentThread().getName() +"：" + "Sleep");
+        try {
+          Thread.sleep(10000);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread() + "：" + a);
+        readLock.unlock();
+      }
+    });
 
+    Thread writeThread = new Thread(new Runnable() {
+      @Override
+      public void run() {
+        System.out.println(Thread.currentThread().getName() +"：" + "invoke");
+        writeLock.lock();
+        System.out.println(Thread.currentThread().getName() +"：" + "getwritelock");
+        System.out.println(Thread.currentThread().getName() +"：" + "Sleep");
+        try {
+          Thread.sleep(10000);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+        a++;
+        System.out.println(Thread.currentThread() + "：" + a);
+        writeLock.unlock();
+      }
+    });
+    readWriteThread.start();
+    writeThread.start();
+    readWriteThread2.start();
+    try {
+      readWriteThread.join();
+      readWriteThread2.join();
+      writeThread.join();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
     }
+  }
 }
