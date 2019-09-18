eb7d1bba3ff966ee7363cf65212da30a74d41a15
yingmule <534446340@qq.com>
2019-09-18 15:53:04 +0800

    Rabbit

---
+++ src/test/java/com/rabbit/api/ack/ConsumerAck2.java
@@ -0,0 +1,33 @@
+package com.rabbit.api.ack;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class ConsumerAck2 {
+
+    public static void main(String[] args)  throws IOException, TimeoutException{
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("192.168.100.153");
+        connectionFactory.setPort(5672);
+        connectionFactory.setVirtualHost("/");
+
+        Connection connection = connectionFactory.newConnection();
+        Channel channel = connection.createChannel();
+
+
+        String exchangeName = "test_ack_exchange";
+        String queueName = "test_ack_queue";
+        String routingKey = "ack.#";
+
+        channel.exchangeDeclare(exchangeName, "topic", true, false, null);
+        channel.queueDeclare(queueName, true, false, false, null);
+        channel.queueBind(queueName, exchangeName, routingKey);
+
+        // 手工签收 必须要关闭 autoAck = false
+        channel.basicConsume(queueName, false, new MyConsumer(channel));
+    }
+}
