379c1d4c36c6ea9cfca6d3bf706146c9846c9c22
yingmule <534446340@qq.com>
2019-12-29 21:37:38 +0800

    修改 2.2.1

--- mycode/purchasing/src/main/java/com/mycode/purchasing/PurchasingApplication.java
+++ mycode/purchasing/src/main/java/com/mycode/purchasing/PurchasingApplication.java
@@ -3,6 +3,9 @@ package com.mycode.purchasing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 代购辅助项目
+ */
 @SpringBootApplication
 public class PurchasingApplication {
 
